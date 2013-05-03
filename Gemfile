785adca023f2c6976829b1fcdf53db5f33664d40
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-03 16:07:06 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+#gem 'sqlite3'
+gem 'mysql2'
+
+
+gem 'sass-rails',   '~> 3.2.3'
+gem 'coffee-rails', '~> 3.2.1'
+
+
+gem 'uglifier', '>= 1.0.3'
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simple::Application.config.secret_token = '7640573e46a9891f65addf6b4de9568bb000895172dba013c6b2de7e07b0581a8f045d8886a0bcd5a1edab49e7dc9e4ceffb7cd36289fd39ddd555fbf661e16a'
