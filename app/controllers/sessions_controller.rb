1bab0fd949a0433ba019fd4d3b230ee8a0ebbb25
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-05 20:13:58 +0300

    added error message for json with what went wrong

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,14 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_username( params[:username] )
+		if ( user.is_nil? )
+			render json: { "loggedIn" => false , "error" => "email" }
+		end
 		if ( user && user.authenticate( params[:password] ) )
 			session[:user_id] = user.id
 			render json: { "loggedIn" => true , "user_id" => user.id , "key" => Password.create( params[:username]+params[:password] ) }
 		else
-			render json: { "loggedIn" => false }
+			render json: { "loggedIn" => false , "error" => "password" }
 		end
 	end
 
