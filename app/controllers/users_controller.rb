9a015ac4a5acf2a0813233684e22460ef4d2d0cc
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-05 23:42:42 +0300

    now returning all users with only email&name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	def index
-		@users = User.all
+		@users = User.select( "username, email" ).all
 		@users.delete_if {|hash| hash["id"] == session[:user_id]}
 		respond_to do |format|
 			format.html # index.html.erb
@@ -9,12 +9,10 @@ class UsersController < ApplicationController
 	end
 
 	def view
-		@user = User.find ( params[:id] )
-		@user.authToken = nil
-		@user.password_digest = nil
+		@user = User.select( "username, email" ).find ( params[:id] )
 		respond_to do |format|
 			format.html # view.html.erb
-			format.json { render json: { "username" => @user.username , "email" => @user.email } }
+			format.json { render json: @user }
 		end
 	end
 
