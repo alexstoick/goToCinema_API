b9bf6ee7b891868097f6b1a59dfb1fe7347f1fa7
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-13 21:21:22 +0300

    updated movie controller with json goodies. deleted comments from user

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,32 +1,15 @@
 class MoviesController < ApplicationController
-  # GET /movies
-  # GET /movies.json
+
   def index
-    @movies = Movie.all
+    movies = Movie.all
 
     json= []
 
-    @movies.each do |movie|
-    	movie.showtimes.each do |showtime|
-    		json.push ( {
-    					"id" 		=> movie.id 	,
-    					"titluRo"	=> movie.titluRo,
-    					"titluEn"	=> movie.titluEn,
-    					"regizor"	=> movie.regizor,
-    					"actori"	=> movie.actori	,
-    					"nota"		=> movie.nota	,
-    					"gen"		=> movie.gen	,
-    					"image"		=> movie.image	,
-    					"ora"		=> showtime.hour,
-    					"cinema"	=> showtime.place } )
+  	render json: movies.to_json( :only => [ :id , :actori , :gen , :image , :nota , :regizor , :titluEn , :titluRo] ,
+  					:include => { :showtimes => { :only => [ :hour , :place ] } } )
 
-    	end
-    end
-	render json: json
   end
 
-  # GET /movies/1
-  # GET /movies/1.json
   def show
     movie = Movie.find(params[:id])
     render json: movie
@@ -34,7 +17,10 @@ class MoviesController < ApplicationController
 
   def aparitii
   	movie = Movie.find(params[:id])
-  	render json: movie.showtimes.select( "hour, place" )
+
+  	render json: movie.to_json( :only => [ :actori , :gen , :image , :nota , :regizor , :titluEn , :titluRo] ,
+  					:include => { :showtimes => { :only => [ :hour , :place ] } } )
+
   end
 
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -14,8 +14,8 @@ class User::UsersController < ApplicationController
 		user = User.find(params[:id])
 		render json: user.to_json( :only => [] , :methods => [:fullname] ,
 									:include => { :posts => { :only => [ :title , :content, :receiver_id] } } )
-		#render json: user.posts.select("title, content, receiver_id")
 	end
+
 	def wall
 		user = User.find(params[:id])
 		#:only => [ :nume , :prenume, :id],
@@ -23,9 +23,6 @@ class User::UsersController < ApplicationController
 									:include =>  { :wall_posts => {
 										:include => { :sender => { :only => [] , :methods => [:fullname] } },
 										:only => [ :title , :content ] } } )
-
-
-		return
 	end
 
 	def search
@@ -35,6 +32,3 @@ class User::UsersController < ApplicationController
 		render json: result
 	end
 end
-  # konata.to_json(:include => { :posts => {
-  #                                :include => { :comments => { :only => :body } },
-  #                                :only => :title } })
