12f383a0ee855d90dfb67d7543722cf597f70966
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-06-04 22:53:12 +0300

    updated queries and also improved sql logic

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -2,7 +2,6 @@ class User::UsersController < ApplicationController
 	def index
 		users = User.select( "username, email, nume, prenume, id" ).find ( :all )
 		render json: users
-
 	end
 
 	def view
@@ -11,17 +10,19 @@ class User::UsersController < ApplicationController
 	end
 
 	def posts
-		user = User.find(params[:id])
+		user = User.where(:id => params[:id]).joins(:posts).includes(:posts)
 		render json: user.to_json( :only => [] , :methods => [:fullname] ,
 									:include => { :posts => { :only => [ :title , :content, :receiver_id] } } )
 	end
 
 	def wall
-		user = User.find(params[:id])
+
+		user = User.where(:id => params[:id]).joins(:wall_posts).includes(:wall_posts).order('posts.created_at DESC')
+
 		render json: user.to_json( :only => [:id , :image , :DOB , :created_at ], :methods => [:fullname] ,
-									:include =>  { :wall_posts => {
+									:include =>  { :wall_posts => { :order => 'created_at DESC',
 										:include => { :sender => { :only => [ :id , :image ] , :methods => [:fullname] } },
-										:only => [ :title , :content ] } } )
+										:only => [ :title , :created_at ] } } )
 	end
 
 	def search
